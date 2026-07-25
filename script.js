f470d533437f947f9ea66554d581e6776e6892b0
Davon <dolewis86@gmail.com>
2026-07-24 23:27:10 -0400

    added renderlist and linked

--- script.js
+++ script.js
@@ -9,28 +9,42 @@
 const taskList = [];
 
 let taskForm = document.getElementById("taskForm");
-
+const taskAdded = document.getElementById("taskAdded");
 
 //Each task should be stored as an object with properties such as task name, category, deadline, and status.
 taskForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const taskName = document.getElementById("taskName").value
-    const category = document.getElementById("category").vallue
-    const deadline = document.getElementById("deadline").value
-    const status = document.getElementById("status").value
-    //note to self: adding .value gets what's being typed inside specific field
+  const taskName = document.getElementById("taskName").value;
+  const category = document.getElementById("category").vallue;
+  const deadline = document.getElementById("deadline").value;
+  const status = document.getElementById("status").value;
+  //note to self: adding .value gets what's being typed inside specific field
 
-    //i believe this next part works because it is only being triggerd when user presses input
+  //i believe this next part works because it is only being triggerd when user presses input
 
-    const userTasks = {
-        name: taskName.trim(),
-        category: category.trim(),
-        deadline: deadline.trim(),
-        status: status.trim(),
-    }
-    
-    //Add the task object to an array that holds all tasks.
-    taskList.push(userTasks);
+  const userTasks = {
+    name: taskName.trim(),
+    category: category.trim(),
+    deadline: deadline.trim(),
+    status: status.trim(),
+  };
 
+  //Add the task object to an array that holds all tasks.
+  taskList.push(userTasks);
+
+  console.log("Updated List:", taskList);
+
+  taskForm.reset();
+
+  renderlist();
 });
+
+function renderlist() {
+  taskAdded.innerHTML = "";
+
+  for (let i = 0; i < taskList.length; i++) {
+    let taskItem = document.createElement("li");
+    taskItem.innerHTML = taskList[i];
+  }
+}
